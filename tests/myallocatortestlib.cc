cf0ce7f5d87471257ee7f097dd25fd8fd33df288
Neil Chen <neilchen1998@gmail.com>
2025-06-15 16:40:22 -0500

    added test cases

--- tests/myallocatortestlib.cc
+++ tests/myallocatortestlib.cc
@@ -4,6 +4,7 @@
 #include <array>    // std::array
 #include <memory_resource>  // std::pmr::monotonic_buffer_resource
 #include <span> // std::span
+#include <stdio.h> // printf
 
 #include "allocator/allocator.hpp"
 
@@ -34,13 +35,21 @@ TEST_CASE( "Elements Less Than Main Buffer", "[main]" )
     {
         REQUIRE(myVec.at(i) == s.at(i));
     }
+
+    for (auto i {0}; i < N; ++i)
+    {
+        REQUIRE(&myVec[i] >= &buffer[0]);
+        REQUIRE((&myVec[i]) < (&backUp[backUpSz - 1]));
+    }
 }
 
 TEST_CASE( "Elements More Than Main Buffer But Less Than the Upstream Buffer", "[main]" )
 {
+    // When we are keep pushing elements into the buffer, the allocator might need to reallocate over and over again
+    // Therefore we need to have more than 26 char spaces so the allocator can reallocate and we will not need to allocate on the heap
     constexpr std::size_t bufferSz = 4;
-    constexpr std::size_t backUpSz = 20;
-    constexpr auto N = (backUpSz - 1);
+    constexpr std::size_t backUpSz = 1024;
+    constexpr auto N = ('z' - 'a' + 1);
 
     std::array<char, bufferSz> buffer;
     std::array<char, backUpSz> backUp;
@@ -62,6 +71,20 @@ TEST_CASE( "Elements More Than Main Buffer But Less Than the Upstream Buffer", "
     for (auto i {0}; i < N; ++i)
     {
         REQUIRE(myVec.at(i) == s.at(i));
+    }
+
+    auto* startPtr = &backUp[0];
+    auto* endPtr = &backUp[backUpSz - 1];
+
+    // printf("start of buffer: 0x{%x}\n", &buffer[0]);
+    // printf("start of backup: 0x{%x}\n", &backUp[0]);
+
+    for (auto i {0}; i < N; ++i)
+    {
+        auto* p = &myVec[i];
+        // printf("cur ptr {%d}: 0x{%x}\n", i, p);
+        REQUIRE(p >= startPtr);
+        REQUIRE(p < endPtr);
     }
 }
 
